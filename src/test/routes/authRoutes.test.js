6de4e638d38537b4c2fa9320ad216ce3eac2ecf5
Jefferson Brandão <jeffersonvideo125@gmail.com>
2024-12-19 14:43:13 -0300

    Testando rota de login

---
+++ src/test/routes/authRoutes.test.js
@@ -0,0 +1,25 @@
+import request from 'supertest';
+import { afterEach, beforeEach, describe } from '@jest/globals';
+import app from '../../app';
+
+let server;
+beforeEach(() => {
+  const port = 3000;
+  server = app.listen(port);
+});
+
+afterEach(() => {
+  server.close();
+});
+
+describe('Testando a rota login (POST)', () => {
+  test('O login deve possuir um email e senha para se autenticar', async () => {
+    const loginMock = { email: 'jeff@teste.com.br' };
+
+    await request(server)
+      .post('/login')
+      .send(loginMock)
+      .expect(500)
+      .expect('"A senha de usuario é obrigatório."');
+  });
+});
